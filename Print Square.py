f9f79d195dc508635b32bc8fe0f9f0f406848584
ArunimaGupta1 <72702440+ArunimaGupta1@users.noreply.github.com>
2021-01-04 12:17:42 +0530

    Create Print Square.py
    Given an integer s, write a program to print the square of size s using * character. 
    
    Before submitting code, verify it by running offline. Does your square visually looks like a square?
    
    Example 1:
    
    Input:
    s = 4
    Output:
    
    
    Explanation:
    It's a square! Each side contains s = 4 *.
    Your Task:
    The function square takes s as a parameter. Use this s to make your square with s *. Print your answer within the function itself. You do need to provide a new line at the end.

---
+++ Print Square.py
@@ -0,0 +1,9 @@
+# geeksforgeeks-practice
+def square(s):
+    for i in range(s):
+        if i == 0:
+            print('* ' * s)
+        elif i == s - 1:
+            print('* ' * s)
+        else:
+            print('*', ' ' * (2*s-3), '*', sep='')
