6256b7faf28351af23c0d31d463adf731f456ad0
ArunimaGupta1 <72702440+ArunimaGupta1@users.noreply.github.com>
2021-01-04 10:03:26 +0530

    Add files via upload

---
+++ largest_of_three.py
@@ -0,0 +1,16 @@
+x = int(input("Enter first number",End=" ")
+y = int(input("Enter second number",End=" "))
+z = int(input("Enter third number",End=" "))
+
+if x>y:
+        if x>z:
+        print(x," is greatest")
+        else  :
+        print(z, " is greatest")
+
+else:
+     if z>y:
+     print(z, " is greatest")
+     else  :
+    print(y, " is greatest")
+     
